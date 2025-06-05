2b718b554e24f49420b4598349fdfb8d036b6776
madankumar2473 <93917005+madankumar2473@users.noreply.github.com>
2025-06-05 12:25:29 +0530

    Delete OneDrive directory

--- OneDrive/Desktop/Leucine/Todo_Backend/src/routes/summaryroutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const summaryrouter = Router();
-import {summarizeAndSend} from '../controllers/summarycontroller.js';
-
-summaryrouter.post('/', summarizeAndSend);
-
-export default summaryrouter;
--- OneDrive/Desktop/Leucine/Todo_Frontend/src/api/summaryapi.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = import.meta.env.VITE_API_URL;
-
-export const generateAndSendSummary = async () => {
-  const response = await axios.post(`${API_URL}/summarize`);
-  return response.data;
-};
